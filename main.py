92093026730d39e24536ba6a31c9368e157f8f74
Romain Antunes <romainantunes2003@hotmail.com>
2023-12-01 13:48:27 +0100

    feat: add optimisation for rotors list

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from enigma.machine import EnigmaMachine
 import time
+from multiprocessing import Process, cpu_count
+
 from alive_progress import alive_bar
+from enigma.machine import EnigmaMachine
 
 
 def chiffrer(rotors, reflecteur, ring, plugboard, text):
@@ -25,7 +27,9 @@ def dechiffrer(rotors, reflecteur, ring, plugboard, text):
     return machine.process_text(text)
 
 
-def brut_force_enigma(message_to_find, rotors="I II III", reflecteur="B", ring="01 01 01", plugboard=""):
+def brut_force_enigma(message_to_find, reflecteur="B", ring="01 01 01",
+                      plugboard=""):
+
     first_word = "meteorologie".upper()
 
     # Vous allez devoir créer une fonction bruteforce qui va tester toutes les possibilités restantes afin de
@@ -35,26 +39,41 @@ def brut_force_enigma(message_to_find, rotors="I II III", reflecteur="B", ring="
     # tout le message.
     temp1 = time.time()
 
-    with alive_bar(26 * 26 * 26 * 26 * 26 * 26 * 26 * 26 * 26 * 26, title="Brut force en cours...") as bar:
-        for rotor1 in ["I", "II", "III", "IV", "V"]:
-            for rotor2 in ["I", "II", "III", "IV", "V"]:
-                for rotor3 in ["I", "II", "III", "IV", "V"]:
-                    for key1 in range(1, 27):
-                        for key2 in range(1, 27):
-                            for key3 in range(1, 27):
+    rotors_check = ["I", "II", "III", "IV", "V"]
+
+    # 5! 2!
+    with alive_bar((5 * 4 * 3) * 26 * 26 * 26, title="Brut force en cours...") as bar:
+        for k, rotor1 in enumerate(rotors_check.copy()):
+            rotor2_list = rotors_check.copy()
+            rotor2_list.pop(k)
+
+            for j, rotor2 in enumerate(rotor2_list):
+                rotor3_list = rotor2_list.copy()
+                rotor3_list.pop(j)
+
+                for rotor3 in rotor3_list:
+
+                    rotors_test = rotor1 + " " + rotor2 + " " + rotor3
+                    machine = EnigmaMachine.from_key_sheet(
+                        rotors=rotors_test,
+                        reflector=reflecteur,
+                        ring_settings=ring,
+                        plugboard_settings=plugboard,
+                    )
+
+                    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                    for key1 in alphabet:
+                        for key2 in alphabet:
+                            for key3 in alphabet:
+                                kenngruppenheft = key1 + key2 + key3
+                                machine.set_display(kenngruppenheft)
+
                                 bar()
 
-                                machine = EnigmaMachine.from_key_sheet(
-                                    rotors=rotor1 + " " + rotor2 + " " + rotor3,
-                                    reflector=reflecteur,
-                                    ring_settings=str(key1) + " " + str(key2) + " " + str(key3),
-                                    plugboard_settings=plugboard,
-                                )
-
                                 message_process = machine.process_text(message_to_find)
-                                if message_process[0:12] == first_word:
+                                if message_process.startswith(first_word):
                                     temp2 = time.time()
-                                    print("Temps de calcul : " + str(temp2 - temp1))
+                                    print("Temps de calcul :", str(temp2 - temp1), "secondes")
 
                                     print("Message trouvé : " + message_process)
                                     print("Rotors utilisés : " + rotor1 + " " + rotor2 + " " + rotor3)
@@ -63,26 +82,47 @@ def brut_force_enigma(message_to_find, rotors="I II III", reflecteur="B", ring="
                                     return
 
     print("Message non trouvé")
-    print("Temps de calcul : " + str(time.time() - temp1))
+    print("Temps de calcul :", str(time.time() - temp1), "secondes")
 
 
-# Config pour le 11 novembre
-rotors = "I V IV"
-reflecteur = "B"
-ring = "13 15 11"
-plugboard = "NX EC RV GP SU DK IT FY BL AZ"
+if __name__ == '__main__':
+    # Config pour le 11 novembre
+    rotors = "I V IV"
+    reflecteur = "B"
+    ring = "13 15 11"
+    plugboard = "NX EC RV GP SU DK IT FY BL AZ"
 
-message = "Les troupes britanniques sont entrees a Cuxhaven a quatorze heures le six mai Desormais tout le trafic radio cessera je vous souhaite le meilleur Fermeture pour toujours tout le meilleur au revoir."
+    message = "Les troupes britanniques sont entrees a Cuxhaven a quatorze heures le six mai Desormais tout le trafic radio cessera je vous souhaite le meilleur Fermeture pour toujours tout le meilleur au revoir."
 
-message_chiffrer = chiffrer(rotors, reflecteur, ring, plugboard, message)
-print(message_chiffrer)
+    message_chiffrer = chiffrer(rotors, reflecteur, ring, plugboard, message)
+    print(message_chiffrer)
 
-message_dechiffrer = dechiffrer(rotors, reflecteur, ring, plugboard, message_chiffrer)
-print(message_dechiffrer)
+    message_dechiffrer = dechiffrer(rotors, reflecteur, ring, plugboard, message_chiffrer)
+    print(message_dechiffrer)
 
-message_brut_force = "UFLTVDIPVYDQFLDZGEHBNLVVPNCMDTJBSBCISSQAJPWTIMJMRPTOMIKKYKGCJXBNKEQHSUAOMGUJOKLSNABOCSOMYGVLXCJCGVAAYSJFOSISJCAIYFHUJYYJDGGWNCZ"
+    # # # # # # # # # # # # # # # # # # # #
+    #            BRUT FORCE               #
+    # # # # # # # # # # # # # # # # # # # #
 
-# Les allemands commencaient toujours le premier message de la journée en annonçant le Wetterbericht soit la météo. Dans ce message ça correspond au mot « meteorologie ».
-# La disposition des rotors est la suivante : 19 6 8
-# Les lettres sont branchées de la façon suivante : GH QW TZ RO IP AL SJ DK CN YM
-brut_force_enigma(message_brut_force, "VI II VIII", "B", "19 6 8", "GH QW TZ RO IP AL SJ DK CN YM")
+    # Les allemands commencaient toujours le premier message de la journée en annonçant le Wetterbericht soit la météo. Dans ce message ça correspond au mot « meteorologie ».
+    # La disposition des rotors est la suivante : 19 6 8
+    # Les lettres sont branchées de la façon suivante : GH QW TZ RO IP AL SJ DK CN YM
+
+    print("Nombre de cpu: ", cpu_count())
+
+    message_brut_force = "UFLTVDIPVYDQFLDZGEHBNLVVPNCMDTJBSBCISSQAJPWTIMJMRPTOMIKKYKGCJXBNKEQHSUAOMGUJOKLSNABOCSOMYGVLXCJCGVAAYSJFOSISJCAIYFHUJYYJDGGWNCZ"
+
+    procs = []
+    rotors_tests = [
+        ["I", "II", "III", "IV", "V"],
+    ]
+
+    for rotor_test in rotors_tests:
+        proc = Process(target=brut_force_enigma, args=(
+            message_brut_force, "B", "19 6 8", "GH QW TZ RO IP AL SJ DK CN YM"))
+
+        procs.append(proc)
+        proc.start()
+
+    for proc in procs:
+        proc.join()
